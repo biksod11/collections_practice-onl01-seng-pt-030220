eab6e1dbdca32587a7b8b65fe7f2ef54c136aee9
Bikram Sodhi <biksodhi@gmail.com>
2020-03-13 21:12:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,4 +13,40 @@ def sort_array_char_count(strings)
     short.length <==> long.length
   end
 end
+
+def swap_elements(swap)
+  array[1], array [2] = array[2], array [1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(kesha)
+  style = []
+  kesha.each do |item|
+    item[2] = "$"
+    style << item
+  end
+end
+
+def find_a(find)
+  find.find_all do |item|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
