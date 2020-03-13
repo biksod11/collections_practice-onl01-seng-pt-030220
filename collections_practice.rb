ef97a9a6dd7f08e369fc31fc732a630c1dc410d9
Bikram Sodhi <biksodhi@gmail.com>
2020-03-13 21:11:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(ints)
+  ints.sort
+end
 
+def sort_array_desc(ints)
+  ints.sort do |left, right|
+    right <==> left
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |short, long|
+    short.length <==> long.length
+  end
+end
+end
